99737a1e10c651a50080768d71e7cf43da60253a
Klaus <klaus4512@outlook.com>
2024-11-29 21:19:02 -0300

    Criação da modelagem de dados do colaborador

---
+++ app/Models/Employee.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Employee extends Model
+{
+    use HasUuids;
+    //
+    protected $table = 'employees';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'document_number',
+        'state',
+        'city',
+    ];
+
+    protected function documentNumber(): Attribute
+    {
+        return Attribute::make(
+            set: static fn (string $value) => preg_replace('/\D/', '', $value)
+        );
+    }
+
+    public function manager(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'manager_id');
+    }
+}
